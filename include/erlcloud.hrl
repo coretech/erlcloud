5f648c9ea136c022cab810e585606634b1b12c67
Dmitriy Kargapolov <dmitriy.kargapolov@idt.net>
2017-05-09 12:43:58 -0400

    Add macros for conditional include of the header
    Somehow this file has been included twice, resulting in compile-time error.

--- include/erlcloud.hrl
+++ include/erlcloud.hrl
@@ -1,2 +1,7 @@
+-ifndef(__erlcloud_hrl__).
+-define(__erlcloud_hrl__, 1).
+
 -type proplist() :: proplists:proplist().
 -type datetime() :: {{pos_integer(), 1..12, 1..31}, {0..23, 0..59, 0..60}}.
+
+-endif.
